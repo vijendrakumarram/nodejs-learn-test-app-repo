1716610a9f1c4190a2a9929224d14d22ab8ec9da
Vijendra Kumar <vijendrakumarram@gmail.com>
2019-10-06 23:13:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/*var io = require('socket.io')(process.env.PORT || 3000);
+console.log('server started....');
+*/
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+http.listen(process.env.PORT || 8080, function(){
+console.log('listening on: 8080');
+});
+
+console.log('Yeh....server is running...');
+
+
